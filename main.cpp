fcb044cefaa5ae5b9d1b40a730f79df06829c3a4
Caleb McCarthy <caleb.andrew.mccarthy@ibm.com>
2021-05-09 16:27:05 -0500

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,42 @@
 #include <iostream>
+#include <memory>
 
-
-class Btree {
-    public:
+struct Btree {
         std::string val;
-        Btree* left;
-        Btree* right;
+        std::unique_ptr<Btree> left;
+        std::unique_ptr<Btree> right;
 
         Btree(std::string val) {
-            this->val = val;
-            this->left = NULL;
-            this->right = NULL;
+            this->val = val; this->left = NULL; this->right = NULL;
         }
-
+        
         int evaluate() {
             if(this->val == "*") {
                 return this->left->evaluate() * this->right->evaluate();
-            }else  if(this->val == "-") {
+            }
+            else  if(this->val == "-") {
                 return this->left->evaluate() - this->right->evaluate();
             }
             else  if(this->val == "/") {
                 return this->left->evaluate() / this->right->evaluate();
-            }else  if(this->val == "+") {
+            }
+            else  if(this->val == "+") {
                 return this->left->evaluate() + this->right->evaluate();
-            } else {
+            } 
+            else {
                 return std::stoi(this->val);
             }
         }
 };
 
 int main() {
-    Btree* root = new Btree("+");
-    root->left = new Btree("*");
-    root->right = new Btree("-");
-    root->left->left = new Btree("5");
-    root->left->right = new Btree("4");
-    root->right->right = new Btree("/");
-    root->right->left = new Btree("100");
-
-    root->right->right->right = new Btree("2");
-    root->right->right->left = new Btree("20");
-
+    std::unique_ptr<Btree> root = std::make_unique<Btree>("*");
+    root->left = std::make_unique<Btree>("+");
+    root->right = std::make_unique<Btree>("+");
+    root->left->left = std::make_unique<Btree>("3");
+    root->left->right = std::make_unique<Btree>("2");
+    root->right->right = std::make_unique<Btree>("5");
+    root->right->left = std::make_unique<Btree>("4");
     std::cout<<"answer:"<<root->evaluate()<<std::endl;
     return 0;
 }
